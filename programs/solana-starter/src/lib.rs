18c56a7cced9cc7ddf9fa6bf084f69e322dd4092
root <contact.krausch@gmail.com>
2022-09-03 14:58:21 +0200

    frontend added

--- programs/solana-starter/src/lib.rs
+++ programs/solana-starter/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("A7HBry8TeHZqgQ8cMeZwVTsE336gwSMGgV75MU6MMb5r");
 
 #[program]
 pub mod solana_starter {
