feb6c2c9b4b615d46ff9f7c22bd4e097736234a3
root <contact.krausch@gmail.com>
2022-09-03 15:49:33 +0200

    Update directory

---
+++ programs/solana-starter/src/space.rs
@@ -0,0 +1,8 @@
+pub const DISCRIMINATOR_LENGTH: usize = 8;
+pub const STRING_LENGTH_PREFIX: usize = 4;
+pub const PUBLIC_KEY_LENGTH: usize = 32;
+pub const EIGHT_BYTES: usize = 1;
+pub const THIRTY_BYTES: usize =4;
+pub const SIXTEEN_BYTES: usize = 2;
+pub const SIXTYFOUR_BYTES: usize = 8; 
+pub const BOOL_LENGTH: usize = 1;
